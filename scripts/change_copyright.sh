877c8eb2d40d75a2ea14635b328b1eaf93dcf5ea
robin@rawsontetley.org <>
2010-01-04 13:48:56 +0000

    Updated copyright to 2010

--- scripts/change_copyright.sh
+++ scripts/change_copyright.sh
@@ -5,7 +5,7 @@ cd ..
 
 # Change all 2000-2008 text to 2000-2009 
 for F in `find src srcui -name '*.java'`; do
-	cat $F | sed 's/2000-2008/2000-2009/g' > $F.tmp
+	cat $F | sed 's/2000-2009/2000-2010/g' > $F.tmp
 	mv $F.tmp $F;
 done
 
