e2497700798d7d26a89948364118a48ae16813ec
Robin Rawson-Tetley <robin@hyper>
2010-03-15 08:43:11 +0000

    *. -D switches are passed through from Windows bootloader now so they -  can be put in the shortcut

--- src/net/sourceforge/sheltermanager/asm/startup/WindowsBootTrans.java
+++ src/net/sourceforge/sheltermanager/asm/startup/WindowsBootTrans.java
@@ -46,6 +46,16 @@ public class WindowsBootTrans {
         String ram = System.getProperty("asm.ram", "96");
         s.append("-Xmx" + ram + "m ");
 
+	// System properties passthrough
+	String[] props = { "asm.log.debug", "asm.fontsize.textarea",
+	    "asm.swingdefault", "asm.shellexecute" };
+	for (int i = 0; i < props.length; i++) {
+            String p = System.getProperty(props[i], "");
+	    if (!p.equals("")) {
+                s.append("-D" + props[i] + "=" + p + " ");
+	    }
+	}
+
         // Classpath
         s.append("-cp \"" + d + ";" + d + "lib\\charting-0.94.jar;" + d +
             "lib\\mysql.jar;" + d + "lib\\postgresql.jar;" + d +
