9c69f357fdae4de3e3a48ed6ca6d6638274f455b
Robin Rawson-Tetley <robin@hyper>
2010-03-29 20:45:00 +0100

    Couple of fixes

--- scripts/asmcsv/org_ruta.py
+++ scripts/asmcsv/org_ruta.py
@@ -4,14 +4,21 @@ import asm
 
 f = open("ruta.txt", "r")
 lines = f.readlines()
+nextid = 20000
 
 for l in lines:
 	l = l.strip()
-	o = asm.Owner()
+	o = asm.Owner(nextid)
+	nextid += 1
 	spsplit = l.split(" ")
 	if len(spsplit) == 2:
-		o.OwnerForenames = spsplit[0]
-		o.OwnerSurname = spsplit[1].replace("-", " ")
+		if spsplit[1].find("-") == -1:
+			o.OwnerForeNames = spsplit[0]
+			o.OwnerSurname = spsplit[1]
+		else:
+			hsplit = spsplit[1].split("-")
+			o.OwnerForeNames = spsplit[0] + " " + hsplit[0]
+			o.OwnerSurname = hsplit[1]
 	if len(spsplit) == 3:
 		o.OwnerForeNames = spsplit[0] + " " + spsplit[1]
 		o.OwnerSurname = spsplit[2]
