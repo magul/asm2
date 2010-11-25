4992c76660c78bab0f973591d1fd1bfaa0eef5ab
robin@rawsontetley.org <>
2010-11-25 12:04:42 +0000

    Start of mobile interface, experimentation with PSP, report engine complete

---
+++ www/config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+DBTYPE = "MYSQL" # MYSQL, SQLITE or POSTGRESQL
+BASE_URL = "http://localhost/sheltermanager/www/"
+BASE_DIR = "/home/robin/workspace/sheltermanager/www/"
+HOST = "localhost"
+USERNAME = "root"
+PASSWORD = "root"
+DATABASE = "asm"
+SQLITE_FILE = ""
+
+
