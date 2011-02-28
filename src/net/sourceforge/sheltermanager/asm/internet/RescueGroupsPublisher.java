76ccfd52e6ad55c65deb8d6273857d19412eeb5e
robin@rawsontetley.org <>
2011-02-28 09:52:29 +0000

    *. Fixed RescueGroups publishing bug that caused a mismatch between -  animalID and the pic1 field if uploadAll was turned off

--- src/net/sourceforge/sheltermanager/asm/internet/RescueGroupsPublisher.java
+++ src/net/sourceforge/sheltermanager/asm/internet/RescueGroupsPublisher.java
@@ -350,17 +350,31 @@ public class RescueGroupsPublisher extends FTPPublisher {
                 dataFile.append("\"" + comm + "\"");
                 comm = null;
 
-                // pic1 - pic4 - if we don't have a picture for it, just leave the
-                // field blank instead
-                for (int i = 1; i <= 4; i++) {
-                    dataFile.append(", ");
-
-                    if (totalimages >= i) {
-                        dataFile.append("\"" + an.getShelterCode() + "-" + i +
-                            ".jpg" + "\"");
-                    } else {
-                        dataFile.append("\"\"");
-                    }
+                // pic1 - pic4
+                if (totalimages > 0) {
+                	// if uploadAll isn't on, there was just one image
+                	// with the sheltercode as the name
+                	if (!publishCriteria.uploadAllImages) {
+                		dataFile.append("\"" + an.getShelterCode() + ".jpg\", \"\", \"\", \"\"");
+                	}
+                	else {
+                		// Upload is on, output an entry for each image
+                		// we uploaded upto a maximum of 4
+		                for (int i = 1; i <= 4; i++) {
+		                    dataFile.append(", ");
+		
+		                    if (totalimages >= i) {
+		                        dataFile.append("\"" + an.getShelterCode() + "-" + i +
+		                            ".jpg" + "\"");
+		                    } else {
+		                        dataFile.append("\"\"");
+		                    }
+		                }
+                	}
+                }
+                else {
+                	// Leave it blank
+                	dataFile.append("\"\", \"\", \"\", \"\"");
                 }
 
                 // Terminate
